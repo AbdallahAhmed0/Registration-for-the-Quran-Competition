e7d0d7285a1135cc7fe4640f45e6379a736a8333
AbdallahAhmed0 <abdallahahmadadel@gmail.com>
2023-05-02 22:18:32 +0300

    edit Routing Module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
 
+const routes: Routes = [
+  { path: '', redirectTo: 'Register', pathMatch: 'full' },
+  {
+    path:"Register",
+    loadChildren:()=>import('./Registeration/registeration.module').then(l => l.RegisterationModule)
+  },
 
+];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
