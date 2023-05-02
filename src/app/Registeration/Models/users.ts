f349ef4dee2bcd238c1c62c631d6bfc59586b3d4
AbdallahAhmed0 <abdallahahmadadel@gmail.com>
2023-05-02 19:19:38 +0300

    Add model users

---
+++ src/app/Registeration/Models/users.ts
@@ -0,0 +1,15 @@
+export interface users{
+  id?: number,
+  firstName: string,
+  lastName: string,
+  age: number,
+  level: number,
+  nationalId: string,
+  image: [
+    string
+  ],
+  darName: string,
+  phone: string,
+  registrationDate: Date
+
+}
