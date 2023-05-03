87e9b16a48298f5e1253fd8be75d6b3b27917226
AbdallahAhmed0 <abdallahahmadadel@gmail.com>
2023-05-03 14:18:51 +0300

    edit ,odel of user

--- src/app/Registeration/Models/users.ts
+++ src/app/Registeration/Models/users.ts
@@ -1,16 +1,11 @@
 export interface users{
-  
+
   id?: number,
   firstName: string,
   lastName: string,
-  age: number,
   level: number,
   nationalId: string,
-  image: [
-    string
-  ],
   darName: string,
   phone: string,
-  registrationDate: Date
 
 }
