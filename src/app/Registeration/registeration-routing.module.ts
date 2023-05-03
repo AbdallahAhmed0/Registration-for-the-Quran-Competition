7c8b440be5ecbb3caa87f3b34dfa4cc1d76c28d1
AbdallahAhmed0 <abdallahahmadadel@gmail.com>
2023-05-03 16:30:24 +0300

    Add component to state register in routing

--- src/app/Registeration/registeration-routing.module.ts
+++ src/app/Registeration/registeration-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RegisterationComponent } from './Components/registeration/registeration.component';
+import { RegisterStateComponent } from './Components/register-state/register-state.component';
 
 const routes: Routes = [
-  {path:"",component:RegisterationComponent}
+  {path:"",component:RegisterationComponent},
+  {path:"state",component:RegisterStateComponent}
 ];
 
 @NgModule({
