7eefa4b5c0db9ecea932e2c9bb5ab6f4721c766c
AbdallahAhmed0 <abdallahahmadadel@gmail.com>
2023-05-03 14:18:26 +0300

    edit form

--- src/app/Registeration/Components/registeration/registeration.component.ts
+++ src/app/Registeration/Components/registeration/registeration.component.ts
@@ -17,13 +17,10 @@ export class RegisterationComponent implements OnInit {
     this.myForm = this.fb.group({
       firstName: ['', Validators.required],
       lastName: ['', Validators.required],
-      age: ['', [Validators.required,Validators.max(20),Validators.min(5)]],
       level: ['', [Validators.required,Validators.max(8),Validators.min(1)]],
       nationalId: ['', [Validators.required,Validators.minLength(14),Validators.maxLength(14)]],
-      image: [''],
       darName: ['', Validators.required],
       phone: ['', [Validators.required,Validators.minLength(11),Validators.maxLength(11)]],
-      registrationDate: ['']
     });
   }
   onSubmit(){
@@ -53,17 +50,11 @@ return this.myForm.get('firstName');
 get lastName(){
   return this.myForm.get('lastName');
 }
-get age(){
-  return this.myForm.get('age');
-}
 get level(){
   return this.myForm.get('lastName');
 }
 get nationalId(){
   return this.myForm.get('nationalId');
-}
-get image(){
-  return this.myForm.get('image');
 }
 get darName(){
   return this.myForm.get('darName');
@@ -71,8 +62,6 @@ get darName(){
 get phone(){
   return this.myForm.get('phone');
 }
-get registrationDate(){
-  return this.myForm.get('registrationDate');
-}
+
 
 }
