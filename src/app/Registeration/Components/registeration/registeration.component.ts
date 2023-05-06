78465052a6615a31b037a9afd6f76e833a5ca964
AbdallahAhmed0 <abdallahahmadadel@gmail.com>
2023-05-06 17:21:04 +0300

    handle error

--- src/app/Registeration/Components/registeration/registeration.component.ts
+++ src/app/Registeration/Components/registeration/registeration.component.ts
@@ -88,10 +88,9 @@ onSubmit(){
       },
       error: (error:any)=>{
         // Handle error
-        // if(error == ' National ID must be unique and consist of 14 digits'){
+        if(error == ' Error: National ID must be unique and consist of 14 digits'){
           this.consoleError = 'الرقم القومي لا يجب أن يكون قد تم التسجيل به مسبقا';
-          console.log(this.consoleError,'   ' ,error);
-        // }
+        }
       }
       }
     this.registerService.addUser(this.myForm.value).subscribe(observer);
