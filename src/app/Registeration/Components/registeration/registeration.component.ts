a9d098a58c7651a2fd94b334218592cb8fec8216
AbdallahAhmed0 <abdallahahmadadel@gmail.com>
2023-05-07 23:57:24 +0300

    edit error to error.message

--- src/app/Registeration/Components/registeration/registeration.component.ts
+++ src/app/Registeration/Components/registeration/registeration.component.ts
@@ -68,7 +68,7 @@ onSubmit(){
       },
       error: (error:any)=>{
         // Handle error
-        if(error.includes('Error: National ID must be unique and consist of 14 digits')){
+        if(error.message.includes('Error: National ID must be unique and consist of 14 digits')){
           this.consoleError = 'الرقم القومي لا يجب أن يكون قد تم التسجيل به مسبقا';
         }
 
