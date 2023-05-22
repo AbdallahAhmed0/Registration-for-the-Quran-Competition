4e8e0513913a3f88e419c09a0754e770c4c2df1e
AbdallahAhmed0 <abdallahahmadadel@gmail.com>
2023-05-22 15:06:18 +0300

    add spinner component

--- src/app/Shared/spinner/spinner.component.ts
+++ src/app/Shared/spinner/spinner.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoaderService } from '../loader.service';
 
 @Component({
   selector: 'app-spinner',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SpinnerComponent implements OnInit {
 
-  constructor() { }
+  constructor(public loader: LoaderService) { }
 
   ngOnInit(): void {
   }
