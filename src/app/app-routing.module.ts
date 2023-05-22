d295e73f31940bc90ae834f41475b621f35e1cf1
AbdallahAhmed0 <abdallahahmadadel@gmail.com>
2023-05-23 00:48:11 +0300

    fix error of style in open project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './dashboard/home/home.component';
 
 const routes: Routes = [
@@ -14,7 +14,9 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    preloadingStrategy: PreloadAllModules // Use PreloadAllModules strategy
+  })],
 
   exports: [RouterModule]
 })
