83c06515956a249eb578752d51283521f7a6757b
AbdallahAhmed0 <abdallahahmadadel@gmail.com>
2023-05-22 23:45:42 +0300

    add spinner by get mathod of http client

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { catchError, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,12 +9,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Registration-for-the-Quran-Competition';
-  loading: boolean = true;
-
+    loading: boolean = true;
+constructor(private http:HttpClient){}
   ngOnInit() {
-    setTimeout(() => {
-      this.loading = false;
-    }, 3000); // Adjust the timeout duration as needed (in milliseconds)
+    this.getDataFromServer();
+  }
+  getDataFromServer() {
+    this.http.get('https://www.ahbabkhatamelmorsalen.com/').pipe(
+      catchError(error => {
+        return throwError('An error occurred while retrieving the data.');
+      })
+    ).subscribe(response => {
+      this.loading=false;
+    });
   }
 
 }
